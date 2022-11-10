146997eb96ec1c37f79d2184a156d4d17d19dfb6
hellonayeon <hellonykwon@gmail.com>
2022-11-10 15:23:23 +0900

    Chore: 패키지 경로 수정 및 미사용 라이브러리 의존성 제거 #KOK-99

--- src/main/java/shop/kokodo/memberservice/security/WebSecurityConfig.java
+++ src/main/java/shop/kokodo/memberservice/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package shop.kokodo.memberservice.config;
+package shop.kokodo.memberservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
