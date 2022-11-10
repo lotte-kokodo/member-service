331bf9f42c43ed9951bf51b3e0f3366749c328fa
gon-pick <opve555@gmail.com>
2022-11-10 15:01:33 +0900

    Feat : 마이페이지 멤버 -> 상품 리뷰 페이징 적용 완료

--- src/main/java/shop/kokodo/memberservice/client/MemberReviewClient.java
+++ src/main/java/shop/kokodo/memberservice/client/MemberReviewClient.java
@@ -4,12 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import shop.kokodo.memberservice.dto.MypageReviewDto;
+import shop.kokodo.memberservice.dto.PageMypageReviewDto;
 
 import java.util.List;
 
 @FeignClient(name="product-service", url = "http://localhost:8001")
 public interface MemberReviewClient {
 
-    @GetMapping("/product-service/review/member/{memberId}")
-    List<MypageReviewDto> findByMemberId(@PathVariable("memberId") long memberId);
+    @GetMapping("/product-service/review/member/{memberId}/{currentpage}")
+    PageMypageReviewDto findByMemberId(@PathVariable("memberId") long memberId, @PathVariable("currentpage") int page);
 }
