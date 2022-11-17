6142cb8fe69691b2ef850ae1c7b0ee522f2c9697
ohjiwon-201733026 <ojw970725@gmail.com>
2022-11-17 19:24:09 +0900

    Test : 레플리케이션 테스트 제외

--- src/test/java/shop/kokodo/memberservice/config/ReplicationConfigTest.java
+++ src/test/java/shop/kokodo/memberservice/config/ReplicationConfigTest.java
@@ -9,45 +9,45 @@ import org.springframework.boot.test.context.SpringBootTest;
 import shop.kokodo.memberservice.dto.MemberDto;
 import shop.kokodo.memberservice.service.MemberService;
 
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@SpringBootTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ReplicationConfigTest {
 
-    @Autowired
-    MemberService memberService;
-
-    MemberDto memberDto;
-
-    final String loginId = "loginId";
-
-    @BeforeEach
-    public void setUp(){
-        memberDto= MemberDto.builder()
-                .loginId(loginId)
-                .name("name0")
-                .email("email@email.com")
-                .password("password")
-                .birthday("2022-01-01")
-                .profileImageUrl("image")
-                .phoneNumber("010-0000-0000")
-                .address("address")
-                .grade("grade")
-                .build();
-    }
-
-    @Test
-    @DisplayName("member DB wrtie ")
-    public void member_DB_wrte(){
-        memberService.createMember(memberDto);
-    }
-
-    @Test
-    @DisplayName("member DB read ")
-    public void member_DB_read(){
-        memberService.createMember(memberDto);
-
-        memberService.getMemberByLoginId(loginId);
-        memberService.getMemberByLoginId(loginId);
-        memberService.getMemberByLoginId(loginId);
-    }
+//    @Autowired
+//    MemberService memberService;
+//
+//    MemberDto memberDto;
+//
+//    final String loginId = "loginId";
+//
+//    @BeforeEach
+//    public void setUp(){
+//        memberDto= MemberDto.builder()
+//                .loginId(loginId)
+//                .name("name0")
+//                .email("email@email.com")
+//                .password("password")
+//                .birthday("2022-01-01")
+//                .profileImageUrl("image")
+//                .phoneNumber("010-0000-0000")
+//                .address("address")
+//                .grade("grade")
+//                .build();
+//    }
+//
+//    @Test
+//    @DisplayName("member DB wrtie ")
+//    public void member_DB_wrte(){
+//        memberService.createMember(memberDto);
+//    }
+//
+//    @Test
+//    @DisplayName("member DB read ")
+//    public void member_DB_read(){
+//        memberService.createMember(memberDto);
+//
+//        memberService.getMemberByLoginId(loginId);
+//        memberService.getMemberByLoginId(loginId);
+//        memberService.getMemberByLoginId(loginId);
+//    }
 }
